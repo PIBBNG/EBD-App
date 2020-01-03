076a06e7d8e579ae181b3a47b47170197c05e906
Lucas Souza <lucas.soaresouza@gmail.com>
2020-01-03 00:10:05 -0300

    Ola pessas estilizado haha

--- App.js
+++ App.js
@@ -8,16 +8,30 @@
  */
 
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class App extends Component {
   render(){
     return (
-      <View>
-        <Text>
+      <View style={styles.container}>
+        <Text style={styles.mainPage}>
           Olá Pessoas!
         </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  mainPage: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
